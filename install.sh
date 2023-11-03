31baa52b81ab32a721c74a7bf0996f3b47382fa2
Janio PG <janio.garcia@raccoon.ag>
2023-11-03 13:28:23 -0300

    Update install.sh
    Update the github repository URL.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DEBUG_LOGS=$HOME/.appDebugLogs/debug_logs_tracking
 ADB_URL_LINUX=https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 ADB_URL_MAC=https://dl.google.com/android/repository/platform-tools-latest-darwin.zip
 OS_INFO=`uname`
+GITHUB_REPO="https://github.com/Raccoon-Monks/Debug-Logs-for-App-Tracking.git"
 
 # COLORS
 GREEN="\033[0;32m"
@@ -44,7 +45,7 @@ function create_appDebugLogs_folder {
 function download_repository {
     echo -e "$ARROW ${GREEN}Baixando repositório:$CLOSE"
     if git --version; then
-        git clone https://github.com/JanioPG/Debug-Logs-Tracking.git $DEBUG_LOGS
+        git clone $GITHUB_REPO $DEBUG_LOGS
 
         if [[ $? -eq 0 ]]; then
             echo -e "${CYAN}\tFeito: repositório baixado!$CLOSE"
